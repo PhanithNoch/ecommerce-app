55baba73462c04d6df82659d9ab5a5a25edcb9e9
PhaNith <nochphanith@gmail.com>
2024-02-24 16:32:22 +0700

    added migration

---
+++ app/Models/Product.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fiilable = [
+        'name',
+        'description',
+        'price',
+        'category_id',
+        'stock_quantity',
+        'image_url',
+        'user_id'
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function orderDetails()
+    {
+        return $this->hasMany(OrderDetail::class);
+    }
+    
+}
