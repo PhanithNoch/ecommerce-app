28e7730b392aae1ce0f126ba8b7a43a58f165d5f
PhaNith <nochphanith@gmail.com>
2024-03-02 16:49:35 +0700

    updated z-index of annoument navigation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,10 +25,62 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     <!-- Styles -->
-    
+
 
     <link rel="stylesheet" href="{{asset('/css/vendor.css')}}">
     <link rel="stylesheet" href="{{asset('/css/style.css')}}">
+    <!-- all css -->
+    <style>
+        .dropdown-navigation-bar{
+            z-index: 9999 !important;
+        }
+        :root {
+            --primary-color: #F76B6A;
+            --secondary-color: #F76B6A;
+
+            --btn-primary-border-radius: 0.25rem;
+            --btn-primary-color: #fff;
+            --btn-primary-background-color: #F76B6A;
+            --btn-primary-border-color: #F76B6A;
+            --btn-primary-hover-color: #fff;
+            --btn-primary-background-hover-color: #F76B6A;
+            --btn-primary-border-hover-color: #F76B6A;
+            --btn-primary-font-weight: 500;
+
+            --btn-secondary-border-radius: 0.25rem;
+            --btn-secondary-color: #00234D;
+            --btn-secondary-background-color: transparent;
+            --btn-secondary-border-color: #00234D;
+            --btn-secondary-hover-color: #fff;
+            --btn-secondary-background-hover-color: #F76B6A;
+            --btn-secondary-border-hover-color: #F76B6A;
+            --btn-secondary-font-weight: 500;
+
+            --heading-color: #000;
+            --heading-font-family: 'Poppins', sans-serif;
+            --heading-font-weight: 700;
+
+            --title-color: #000;
+            --title-font-family: 'Poppins', sans-serif;
+            --title-font-weight: 400;
+
+            --body-color: #000;
+            --body-background-color: #fff;
+            --body-font-family: 'Poppins', sans-serif;
+            --body-font-size: 14px;
+            --body-font-weight: 400;
+
+            --section-heading-color: #000;
+            --section-heading-font-family: 'Poppins', sans-serif;
+            --section-heading-font-size: 48px;
+            --section-heading-font-weight: 600;
+
+            --section-subheading-color: #000;
+            --section-subheading-font-family: 'Poppins', sans-serif;
+            --section-subheading-font-size: 16px;
+            --section-subheading-font-weight: 400;
+        }
+    </style>
     @livewireStyles
 </head>
 
@@ -36,6 +88,7 @@
     <x-banner />
 
     <body class="font-light antialiased">
+        @include('layouts.partials.announment')
         @include('layouts.partials.header')
         @yield('hero')
 
@@ -46,9 +99,9 @@
 
     @livewireScripts
 
-       <!-- all js -->
-       <script src="{{asset('/js/vendor.js')}}"></script>
-       <script src="{{asset('/js/main.js')}}"></script>
+    <!-- all js -->
+    <script src="{{asset('/js/vendor.js')}}"></script>
+    <script src="{{asset('/js/main.js')}}"></script>
 </body>
 
 </html>
